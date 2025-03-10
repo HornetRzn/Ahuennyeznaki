ea6886e66ecc6a30490d712b559e90eaee8334cb
HornetRzn <hornetrzn@gmail.com>
2025-03-10 19:50:35 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,9 +3,5 @@ import os
 # Токен от @BotFather
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN", "вставь_свой_токен_здесь")
 
-# Настройки вебхука
-WEBHOOK_URL = os.getenv("WEBHOOK_URL", "https://your-render-service.onrender.com")  # Замени на свой URL
-WEBHOOK_PATH = f"/webhook/{TELEGRAM_TOKEN}"
-
 # Лимит сообщений в чате
 MAX_CHAT_MESSAGES = 5
