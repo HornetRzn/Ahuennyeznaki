f312cecb4a9f743f450e373b000600141e0a72d9
HornetRzn <hornetrzn@gmail.com>
2025-03-10 19:52:01 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 # Токен от @BotFather
-TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN", "вставь_свой_токен_здесь")
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN", "7381844150:AAEzGVuelgai3rlSIN3giBuc6A948FDHPJk")
 
 # Лимит сообщений в чате
 MAX_CHAT_MESSAGES = 5
